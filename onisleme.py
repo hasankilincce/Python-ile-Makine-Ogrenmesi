887f7258be8b64bc41ee288813ee0483d4c700d7
Hasan Kılınç <hasankilincce@gmail.com>
2023-09-02 10:01:15 +0300

    2.gun (1)

--- onisleme.py
+++ onisleme.py
@@ -41,7 +41,7 @@ Kategorik değişkenler 2 türdür: Nominal, Ordinal
 Nominal : Hiçbir sırlaması olmayan değerler (Cinsiyet)
 Ordinal : Sıralanabilir ama bu sıralama bir anlam ifade etmez (Plaka kodu)
 """
-# Encoding kullanacağız
+# Encoding kullanacağız: Kategorik => Numeric
 ulke = data.iloc[:,0:1].values                     # Ulke column'u çekme
 
 from sklearn import preprocessing
@@ -59,19 +59,34 @@ ulke = ohe.fit_transform(ulke).toarray()
 
 
 # Veri Birleştirme ve DataFrame Oluşturma
-sonuc = pd.DataFrame(data=ulke, index= range(22), columns= ['fr', 'tr', 'us'])       # Ulke verisini DataFrame yapma
+sonuc = pd.DataFrame(data=ulke, index= range(22), columns= ['fr', 'tr', 'us'])                  # Ulke verisini DataFrame yapma
 
-sonuc2 = pd.DataFrame(data= yas, index= range(22), columns= ['boy', 'kilo', 'yas'])  # Boy verisini DataFrame yapma
+sonuc2 = pd.DataFrame(data= yas, index= range(22), columns= ['boy', 'kilo', 'yas'])             # Boy verisini DataFrame yapma
 
 cinsiyet = data.iloc[:,-1].values
 
-sonuc3 = pd.DataFrame(data = cinsiyet, index = range(22), columns= ['cinsiyet'])     # Cinsiyet verisini DataFrame yapma
+sonuc3 = pd.DataFrame(data = cinsiyet, index = range(22), columns= ['cinsiyet'])                # Cinsiyet verisini DataFrame yapma
 
-s = pd.concat([sonuc, sonuc2], axis = 1)                                             # İki dataframe'i birleştirme
+s = pd.concat([sonuc, sonuc2], axis = 1)                                                        # İki dataframe'i birleştirme
 
-s2 = pd.concat([s, sonuc3], axis = 1)                                                # İki dataframe'i birleştirme
+s2 = pd.concat([s, sonuc3], axis = 1)                                                           # İki dataframe'i birleştirme
 
 
+
+# Veri kümelerinin Eğitim ve Test diye bölünmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(s, sonuc3, test_size=0.33, random_state=42) # 66-33 oranında eğitim ve test olarak bölmek
+
+
+# Öznitelik Ölçekleme
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
 
 
 
